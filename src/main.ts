b2adc7a60a933218eed247fa9e4e3b8adb3db2a8
Marc Schleeweiß <mschleeweiss@gmail.com>
2021-05-21 21:57:39 +0200

    socket io 4

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { SocketIoAdapter } from './socket-io.adapter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useWebSocketAdapter(new SocketIoAdapter(app));
   await app.listen(3000);
 }
 bootstrap();
